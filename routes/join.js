74ba0adba3506f4aa43db7b42383e7b0336ec5b0
sojlee <720sojung@gmail.com>
2020-06-11 21:48:26 +0900

    암호화된 비밀번호로 회원가입, 로그인 기능 구현

--- routes/join.js
+++ routes/join.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+const crypto = require('crypto');
 var dbConfig = require('./dbConfig');
 var pool = mysql.createPool({
 	connectionLimit: 5,
@@ -19,8 +20,36 @@ router.get('/', function(req, res, next) {
 /* Post users listing. */
 router.post('/', function(req, res, next) {
   console.log('req.body : ' + JSON.stringify(req.body));
+	var id = req.body.id;
+	var name = req.body.name;
+	var birth = req.body.birth;
+	var email = req.body.email;
+	var phone = req.body.phone;
+	var inputPassword = req.body.passwd;
+	var salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  var hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+	var address1 = req.body.address1;
+	var address2 = req.body.address2;
+	var friend = req.body.friend;
+	var datas = [id, name, email, phone, birth, address1, hashPassword, address2, salt];
+	console.log(datas);
   // mysql상으로 확인
-	res.json(req.body);
+	pool.getConnection(function(err, conn) {
+		if(err) throw err;
+ 		var sqlForInsertUser = "insert into user(id, name, email, phone, birth, address1, passwd, address2, salt) values(?,?,?,?,?,?,?,?,?)";
+ 		conn.query(sqlForInsertUser, datas, function (err, rows) {
+ 			if(err) console.log("err : " + err);
+ 			console.log("rows : " + JSON.stringify(rows));
+
+			if(rows.affectedRows==0){
+ 				res.send("<script> alert('관리자에게 문의하세요.'); history.back();</script>");
+ 			}
+ 			else{
+ 				 res.send("<script> alert('가입되었습니다.'); location.href='/';</script>");
+ 			}
+ 			conn.release();
+ 		});
+ 	});
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+const crypto = require('crypto');
 var dbConfig = require('./dbConfig');
 var pool = mysql.createPool({
 	connectionLimit: 5,
@@ -19,8 +20,29 @@ router.get('/', function(req, res, next) {
 /* Post users listing. */
 router.post('/', function(req, res, next) {
   console.log('req.body : ' + JSON.stringify(req.body));
-  // mysql상으로 확인
-	res.json(req.body);
+	var id = req.body.id;
+	var inputPassword = req.body.password;
+
+	pool.getConnection(function(err, conn) {
+		if(err) throw err;
+		var sqlForInsertUser = "select salt, passwd from user where id = ?";
+		conn.query(sqlForInsertUser, id, function (err, rows) {
+			if(err) console.log("err : " + err);
+			console.log("rows : " + JSON.stringify(rows));
+			var salt = rows[0].salt;
+			var passwd = rows[0].passwd;
+			var hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+			if(passwd === hashPassword){
+				res.send("<script> alert('로그인 성공'); location.href='/';</script>");
+			}
+			else{
+				res.send("<script> alert('아이디 혹은 비밀번호가 일치하지 않습니다.'); history.back();</script>");
+			}
+
+			conn.release();
+		});
+	});
 });
 
 module.exports = router;
