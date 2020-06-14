771e485293265045d83cf5c83e2f17329c53f0dc
sojlee <720sojung@gmail.com>
2020-06-14 18:22:21 +0900

    상품 목록 보기 기능 구현

--- routes/product.js
+++ routes/product.js
@@ -13,7 +13,31 @@ var pool = mysql.createPool({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('./shop/shop_01.html', {session:req.session});
+	pool.getConnection(function (err, connection) {
+				if(err) throw err;
+				// Use the connection
+				var sqlForSelectList = "select * from product where p_code < 2000";
+				connection.query(sqlForSelectList, function (err, rows){
+						if (err) console.error("err : " + err);
+						console.log("rows : " + JSON.stringify(rows));
+						var p_code = new Array();
+						var p_name = new Array();
+						var p_desc = new Array();
+						var p_price = new Array();
+						var img = new Array();
+						for(var i = 0; i < rows.length; i ++){
+							p_code[i] = rows[i].p_code;
+							p_name[i] = rows[i].p_name;
+							p_desc[i] = rows[i].p_desc;
+							p_price[i] = rows[i].p_price;
+							img[i] = rows[i].product_img;
+						}
+						res.render('./shop/shop_01.html', {session:req.session, p_code:p_code, p_name:p_name, p_desc:p_desc, p_price:p_price, img:img});
+						connection.release();
+
+						// Don't use the connection here, it has been returned to the pool.
+				});
+		});
 });
 
 
