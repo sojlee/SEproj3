5c9cb1e16a85914cabd76d908e7cc90a88fd5e3f
sojlee <720sojung@gmail.com>
2020-06-15 03:24:03 +0900

    구매하기 페이지 구현

--- routes/checkout.js
+++ routes/checkout.js
@@ -13,7 +13,22 @@ var pool = mysql.createPool({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('./shop/checkout.html', {session:req.session});
+	var total = 0;
+	pool.getConnection(function (err, connection) {
+	// review 테이블에 insert한다.
+		var selectreview = 'select p.p_name, p.p_price from product as p join mycart as c on p.p_code = c.product_p_code where c.user_id = ?';
+		connection.query(selectreview, req.session.uid, function (err, rows) {
+						if (err) console.error("err : " + err);
+						console.log("rows : " + JSON.stringify(rows));
+						console.log(rows[0].p_name);
+						console.log(rows[1].p_price);
+						for (var i = 0; i < rows.length; i ++){
+							total += rows[i].p_price;
+						}
+						res.render('./shop/checkout.html', {session:req.session, rows:rows, total:total});
+						connection.release();
+		});
+	});
 });
 
 module.exports = router;
