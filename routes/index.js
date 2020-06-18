7c96ca6fae36b13d853e0b68a439d194f97fc915
sojlee <720sojung@gmail.com>
2020-06-19 01:12:23 +0900

    상품 추천, 랭킹 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,31 @@ var pool = mysql.createPool({
 	port: dbConfig.port,
 	user: dbConfig.user,
 	password: dbConfig.password,
-	database: dbConfig.database
+	database: dbConfig.database,
+	multipleStatements : true
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('./shop/index.html', {session : req.session})
+	//res.render('./shop/index.html', {session : req.session})
+	pool.getConnection(function (err, connection) {
+	// review 테이블에 insert한다.
+		//var selectNew = 'select * from product order by update_date DESC;';
+		var selectGood = 'select p.item_img, p.p_code, p.p_name, avg(r.score) as score from product as p join review as r on p.p_code = r.product_p_code group by (r.product_p_code) order by score desc;';
+		var selectNew = 'select item_img from product order by update_date DESC;';
+		connection.query(selectGood + selectNew, function (err, rows) {
+						if (err) console.error("err : " + err);
+						console.log("rows : " + JSON.stringify(rows));
+						var goodproduct = rows[0];
+						var newproduct = rows[1];
+						console.log(goodproduct);
+						console.log(newproduct);
+						console.log(goodproduct[0].item_img);
+						console.log(newproduct[0].item_img);
+						res.render('./shop/index.html', {session : req.session, newimg:newproduct, goodimg:goodproduct});
+						connection.release();
+		});
+	});
 });
 
 module.exports = router;
